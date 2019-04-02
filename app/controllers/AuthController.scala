6265efbf40c2b79b985cc37b4ef4584029f48769
Jasonvdm <jasonvdm@strava.com>
2019-04-01 21:14:44 -0700

    First auth API endpoint

---
+++ app/controllers/AuthController.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.mvc._
+
+@Singleton
+class AuthController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def authenticatePlayer() = Action { implicit request: Request[AnyContent] =>
+    System.out.println(request.hasBody)
+    System.out.println(request.body.asJson)
+
+    val jsonDump = request.body.asJson
+    if (jsonDump.isDefined) {
+      val name: String = (jsonDump.get \ "username").as[String]
+      val code: Int = (jsonDump.get \ "code").as[Int]
+      System.out.println(name)
+      System.out.println(code)
+    }
+    Ok("yes")
+  }
+}
